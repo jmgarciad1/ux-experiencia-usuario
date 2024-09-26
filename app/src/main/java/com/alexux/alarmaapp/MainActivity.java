7db232c182e13f9126ce5734b60fa7e09ba66861
Alex Meneses <a.menesess@uniandes.edu.co>
2024-09-26 16:57:22 -0500

    Pantallas mobile alex

---
+++ app/src/main/java/com/alexux/alarmaapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.alexux.alarmaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.volver), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void irpantalla2(View vista) {
+        Intent miIntent = new Intent(this, pantalla2.class);
+        startActivity(miIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+}
---
+++ app/src/main/java/com/alexux/alarmaapp/pantalla4.java
@@ -0,0 +1,40 @@
+package com.alexux.alarmaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class pantalla4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pantalla4);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.volver), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void volverpantalla3(View vista) {
+        Intent miIntent = new Intent(this, pantalla3.class);
+        startActivity(miIntent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+
+    }
+
+    public void irpantalla5(View vista) {
+        Intent miIntent = new Intent(this, pantalla5.class);
+        startActivity(miIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+    }
+}
